4ea582230c4ebad62e4865477a6af3dd7272eb4a
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-12-08 15:44:29 +0100

    Created class to keep data about company.

---
+++ src/main/java/com/example/demo/Company.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+public class Company {
+    private String name;
+    private String address;
+    private String email;
+    private String telephone;
+
+    public Company(String name, String address, String email, String telephone) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.telephone = telephone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
+}
