3ada303e8559136729e84b256b61ae099b5d93a6
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-12-22 14:35:50 +0100

    Added possibility of passing data from controller to html file.

--- src/main/java/com/example/demo/VCardController.java
+++ src/main/java/com/example/demo/VCardController.java
@@ -5,27 +5,36 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class VCardController {
 
     @GetMapping("/find")
-    public String find(@RequestParam String profession) throws IOException {
+    public String find(@RequestParam String profession, Model model) throws IOException {
         String address = "https://panoramafirm.pl/szukaj?k=" + profession;
         Document doc = Jsoup.connect(address).get();
 
         Elements companies = doc.select("li.card");
 
+        List<Company> companyList = new ArrayList<>();
+
         for (Element company : companies) {
-            System.out.println(company.select("a.company-name").text());
-            System.out.println(company.select("div.address").text());
-            System.out.println(company.select("a.icon-telephone").attr("title"));
-            System.out.println(company.select("a.ajax-modal-link").attr("data-company-email"));
+
+            companyList.add(new Company(
+                    company.select("a.company-name").text(),
+                    company.select("div.address").text(),
+                    company.select("a.ajax-modal-link").attr("data-company-email"),
+                    company.select("a.icon-telephone").attr("title")));
         }
+
+        model.addAttribute("companyList", companyList);
 
         return "companies";
     }
